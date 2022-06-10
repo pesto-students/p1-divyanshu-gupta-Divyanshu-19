5e6001cb5645443c4c365ba587ec47fafa3d12b4
unknown <guptadivyanshu2012@gmail.com>
2022-06-10 19:58:11 +0530

    Added url validation, loading and Readme

--- Week-10/shortly/src/components/Shortener.js
+++ Week-10/shortly/src/components/Shortener.js
@@ -3,13 +3,31 @@ import styles from './Shortener.module.css';
 
 export default function Main() {
   const [url, setUrl] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
   const [shortenLink, setShortenLink] = useState('');
   async function handleSubmit() {
+    setLoading(true);
+    const re = new RegExp(
+      '((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)'
+    );
+    const validUrl = re.test(url);
+    if (!validUrl) {
+      setError(
+        'URL is not valid. A example of valid url is https://google.com'
+      );
+      setInterval(() => {
+        setError('');
+      }, 5000);
+
+      return;
+    }
+
     const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
     const data = await result.json();
+    setLoading(false);
     setShortenLink(data.result.short_link);
   }
-  console.log(styles);
   return (
     <>
       <div className={styles.mainInput}>
@@ -24,7 +42,8 @@ export default function Main() {
           Shorten It!
         </button>
       </div>
-
+      {error}
+      {loading ? <>Loading...</> : <></>}
       {shortenLink ? (
         <div className={styles.container}>
           <div className={styles.secondContainer}>
